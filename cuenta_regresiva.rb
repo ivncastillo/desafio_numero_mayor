b9576521ed5e7d71f82eb74bce9d99c86f761080
ivncastillo <ivncastillo@gmail.com>
2020-10-23 11:28:21 -0300

    tercer commit

---
+++ cuenta_regresiva.rb
@@ -0,0 +1,13 @@
+var1 = ARGV[0].to_i
+var2 = ARGV[1].to_i
+var3 = ARGV[2].to_i
+
+
+if var1>=var2 && var1>=var3
+    puts var1
+    elsif var2>=var1 && var2>=var3
+    puts var2
+    else
+    puts var3
+end
+
